f5e5093f8b9c43409d89cf5790021c0ded6e3b59
Divya Raj <divya21raj@gmail.com>
2019-03-14 16:58:31 +0530

    Initial Postcard UI created -Base Layout Created, icons added

---
+++ app/src/main/java/com/studioseven/postcard/Utils/SquareImageView.kt
@@ -0,0 +1,18 @@
+package com.studioseven.postcard.Utils
+
+import android.content.Context
+import android.support.v7.widget.AppCompatImageView
+import android.util.AttributeSet
+
+class SquareImageView : AppCompatImageView {
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
